bccb716db35259bb72818925b015693b11bee7c7
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-03-31 01:17:33 +0200

    Refactor splash view and packages

--- lib/views/splash_view.dart
+++ lib/views/splash_view.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:nectarapp/constants.dart';
-import 'package:nectarapp/widgets/splash_body.dart';
-import 'package:nectarapp/widgets/splash_packeges.dart';
-
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+import 'package:nectarapp/views/home_view.dart';
+import '../constants.dart';
+import '../widgets/splash_body.dart';
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SplashDetails(),
+    return FlutterSplashScreen.fadeIn(
+      backgroundColor: kPrimaryColor,
+      childWidget: const SplashViewBody(),
+      duration: const Duration(seconds: 5),
+      animationDuration: Durations.extralong1,
+      nextScreen: const HomeView(),
     );
   }
 }
