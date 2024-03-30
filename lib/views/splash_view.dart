5f00304adf36c4de1ba6b569c0f7cae80948be8f
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-03-31 01:08:41 +0200

    finish splash screen using another splash screen packege

--- lib/views/splash_view.dart
+++ lib/views/splash_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nectarapp/constants.dart';
 import 'package:nectarapp/widgets/splash_body.dart';
+import 'package:nectarapp/widgets/splash_packeges.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
@@ -8,8 +9,7 @@ class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      backgroundColor: kPrimaryColor,
-      body: SplashViewBody(),
+      body: SplashDetails(),
     );
   }
 }
