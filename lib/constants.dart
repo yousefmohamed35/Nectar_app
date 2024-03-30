bcbaed7829f8d2a0d065ef94b60ec3b1178fa3f5
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-03-31 00:43:02 +0200

    add logo and add component of splash screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const Color kPrimaryColor = Color(0xff53B175);
+const String logo = 'assets/images/logo.png';
--- lib/views/splash_view.dart
+++ lib/views/splash_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nectarapp/constants.dart';
+import 'package:nectarapp/widgets/splash_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
@@ -8,6 +9,7 @@ class SplashView extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: kPrimaryColor,
+      body: SplashViewBody(),
     );
   }
 }
