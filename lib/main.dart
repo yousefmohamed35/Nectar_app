5c1afd28e95c546466f850cc28a997fd1cf641af
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-03-30 23:28:29 +0200

    create app and create splash screen

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'views/splash_view.dart';
+
+void main() {
+  runApp(const NectarApp());
+}
+
+class NectarApp extends StatelessWidget {
+  const NectarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
+  }
+}
