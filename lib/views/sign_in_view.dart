e4847c1666937c67aadb896b1d084c2ccddd0b16
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-04-17 14:39:15 +0200

    Update SignInView to StatefulWidget and add password visibility toggle in CustomTextField. Fix layout issues in SignInView.

--- lib/views/sign_in_view.dart
+++ lib/views/sign_in_view.dart
@@ -6,9 +6,15 @@ import 'package:nectarapp/widgets/custom_text_button.dart';
 import 'package:nectarapp/widgets/custom_text_dont_have_account.dart';
 import 'package:nectarapp/widgets/description_text.dart';
 import 'package:nectarapp/widgets/header_text.dart';
-class SignInView extends StatelessWidget {
+class SignInView extends StatefulWidget {
   const SignInView({super.key});
 
+  @override
+  State<SignInView> createState() => _SignInViewState();
+}
+
+class _SignInViewState extends State<SignInView> {
+  var isObscure = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,27 +28,42 @@ class SignInView extends StatelessWidget {
             fit: BoxFit.fill
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-          const  HeaderText( text: 'Loging',),
-            SizedBox(height: 1.sp * 5,),
-          const  DescriptionText(text: 'Enter your emails and password'),
-            SizedBox(height: 1.sp * 20,),
-          const  CustomTextField(text: 'Email',),
-            SizedBox(
-              height: 1.sp * 20,
-            ),
-          const  CustomTextField(text: 'Password',),
-          const  CustomTextButtonForgetPassword(),
-            SizedBox(height: 1.sp * 10,),
-          const  CustomButton(text: 'log in'),
-            SizedBox(height: 1.sp * 20,),
-          const  CustomDontHaveAccount(),
-      
-      
-          ],
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+                SizedBox(height: 1.sp *50,),
+              Align(child: Image.asset('assets/images/carrot.png')),
+              SizedBox(height: 1.sp *150,),
+            const  HeaderText( text: 'Loging',),
+              SizedBox(height: 1.sp * 5,),
+            const  DescriptionText(text: 'Enter your emails and password'),
+              SizedBox(height: 1.sp * 20,),
+            const  CustomTextField(text: 'Email',),
+              SizedBox(
+                height: 1.sp * 10,
+              ),
+              CustomTextField(
+                isObscure: isObscure,
+                text: 'Password',
+              onPressed: () {
+              isObscure = !isObscure;
+              setState(() {
+                
+              });
+            },),
+            const  CustomTextButtonForgetPassword(),
+              SizedBox(height: 1.sp * 10,),
+            const  CustomButton(text: 'log in'),
+              SizedBox(height: 1.sp * 20,),
+            const  CustomDontHaveAccount(),
+                
+                
+            ],
+          ),
         ),
       ),
     );
