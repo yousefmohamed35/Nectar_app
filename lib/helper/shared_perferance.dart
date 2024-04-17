213542a7786f78dbea8d1fc3a0e471587a6f3fcc
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-04-17 15:49:07 +0200

    Add SharedPreferenceHelper class for managing shared preferences

---
+++ lib/helper/shared_perferance.dart
@@ -0,0 +1,27 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPereferenceHelper {
+  static late SharedPreferences sharedPreferences;
+  init() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+  }
+
+  dynamic getData({ required String key}) {
+    return sharedPreferences.getString(key);
+  }
+Future<bool>saveData({ required String key, required dynamic value}) async {
+    if(value is bool) {
+    return await sharedPreferences.setBool(key, value);
+    }
+  else  if(value is String) {
+    return await sharedPreferences.setString(key, value);
+    }
+    else if (value is int) {
+      return await sharedPreferences.setInt(key, value);
+    }
+    else{
+      return await sharedPreferences.setDouble(key, value);
+    }
+  }
+  
+}
