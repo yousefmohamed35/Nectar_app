dc17a55d38b054cc84c740a6c7a766e8b0b302fe
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-04-24 21:55:00 +0200

    refactor custom already have account

--- lib/widgets/custom_already_have_account.dart
+++ lib/widgets/custom_already_have_account.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nectarapp/constants.dart';
 import 'package:nectarapp/views/sign_in_view.dart';
@@ -13,21 +12,21 @@ class CustomAlreadyHaveAccount extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
          Text('Already have an account?',style: TextStyle(
-            fontSize: 1.sp*16,
+            fontSize: 16.sp,
           fontFamily: 'Gilroy-Medium',
           fontWeight: FontWeight.w600,
           )),
-        GestureDetector(
-          onTap: (){
-            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const SignInView(),));
-          },
-          child: Text('Signin',style: TextStyle(
-              color: kPrimaryColor,
-              fontSize: 1.sp*16,
-              fontFamily: 'Gilroy-Medium',
-              fontWeight: FontWeight.w600,
-              ),),
-        ),
+        TextButton( 
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.zero),
+      child:  Text('Signin',style: TextStyle(
+            color: kPrimaryColor,
+            fontSize: 16.sp,
+            fontFamily: 'Gilroy-Medium',
+            fontWeight: FontWeight.w600,
+            ),), onPressed: () {  
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const SignInView(),));
+            },),
       ],
     );
   }
