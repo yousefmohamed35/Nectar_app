453caee93dc621bda1551804e86f5d67a464e98d
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-04-27 21:24:42 +0300

    create sign up model

---
+++ lib/models/sign_up.dart
@@ -0,0 +1,28 @@
+import 'package:nectarapp/api/end_points.dart';
+
+class SignUpModel {
+  final bool isAuthenticated;
+  final String id;
+  final String email;
+  final String token;
+  final String userName;
+  final String phoneNumber;
+
+  SignUpModel(
+      {required this.isAuthenticated,
+      required this.id,
+      required this.email,
+      required this.token,
+      required this.userName,
+      required this.phoneNumber});
+  factory SignUpModel.fromjson(Map<String, dynamic> json) {
+    return SignUpModel(
+      isAuthenticated: json['data'][ApiKeys.isAuthenticated],
+      id: json['data'][ApiKeys.id],
+      email: json['data'][ApiKeys.email],
+      token: json['data'][ApiKeys.token],
+      userName: json['data'][ApiKeys.userName],
+      phoneNumber: json['data'][ApiKeys.phoneNumber],
+    );
+  }
+}
