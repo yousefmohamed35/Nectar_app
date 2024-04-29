771dc6bfdf3ebc4de7712b867767b0a4815c9a0e
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-04-29 23:35:31 +0300

    Update HomeView layout and add search functionality

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return  Scaffold(
+      body: Column(
+        children: [
+          Image.asset('assets/images/carrot.png'),
+          TextField(
+            decoration: InputDecoration(
+            filled: true,
+              fillColor: Color(0xffF2F3F2),
+              labelText: 'Search Store',
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(color: Color(0xffF2F3F2)),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(color: Color(0xffF2F3F2)),
+            ),
+              prefixIcon: Icon(Icons.search_rounded),
+            ),
+          )
+        ]
+      ),
     );
   }
 }
