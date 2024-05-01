b2d6b1c7f8377ea10ff1eb64ea636e0302b5c30d
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-05-01 11:46:17 +0300

    refacor custom item

--- lib/widgets/custom_item.dart
+++ lib/widgets/custom_item.dart
@@ -8,51 +8,48 @@ class CustomItemCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 250.w,
+    width: 173.32.w,
+      
       padding: EdgeInsets.all(16.sp),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
         border: Border.all(color: Colors.grey.shade400),
         color: Colors.white,
       ),
-      child: Card(
-        elevation: 0,
-        color: Colors.transparent,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            
-           Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-             children: [
-               Image.asset('assets/images/banana.png',height: 100.h,width: 100.h,),
-             ],
-           ),  
-            SizedBox(height:50.h),
-            Text('Organic Banana',style: TextStyle(fontSize: 18.sp,fontWeight: FontWeight.w600,),),
-            Text('7pcs, priceg',style: TextStyle(fontSize: 14.sp,color: kSecondaryColor),),
-              SizedBox(height: 20.h,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('\$4.99',style: TextStyle(fontSize: 18.sp,fontWeight: FontWeight.w600),),
-                Container(
-                  height: 60.h,
-                  width: 60.h,
-                  decoration: BoxDecoration(
-                    color: kPrimaryColor,
-                    borderRadius: BorderRadius.circular(20),
-        
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+           children: [
+             Image.asset('assets/images/banana.png',height: 79.43.h,width: 100.h,),
+           ],
+         ),  
+          SizedBox(height: 20.h,),
+          Text('Organic Banana',style: TextStyle(fontSize: 18.sp,fontWeight: FontWeight.w600,),),
+          Text('7pcs, priceg',style: TextStyle(fontSize: 14.sp,color: kSecondaryColor),),
+            SizedBox(height: 20.h,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('\$4.99',style: TextStyle(fontSize: 18.sp,fontWeight: FontWeight.w600),),
+              Container(
+              //  alignment: Alignment.center,
+                height: 50.w,
+                width: 45.67.w,
+                decoration: BoxDecoration(
+                  color: kPrimaryColor,
+                  borderRadius: BorderRadius.circular(15),
+      
+                ),
+                child: IconButton(
+                  onPressed: (){}, icon: Icon(Icons.add,color: Colors.white,size: 32.sp,),
                   ),
-                  child: IconButton(
-                  
-                    onPressed: (){}, icon: Icon(Icons.add,color: Colors.white,size: 32.sp,),
-                    ),
-                )
-              ]
-            ),
-          ]
-        ),
+              )
+            ]
+          ),
+        ]
       ),
     );
   }
