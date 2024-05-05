e5d760f364688e608828797aaa6d8ec0cf97f335
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-05-05 16:44:06 +0300

    convert custom add and decrease to stateful widget and add buttons logic

--- lib/widgets/custom_add_and_decrease_item.dart
+++ lib/widgets/custom_add_and_decrease_item.dart
@@ -2,25 +2,52 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nectarapp/constants.dart';
 
-class CustomAddAndDecreaseItem extends StatelessWidget {
+class CustomAddAndDecreaseItem extends StatefulWidget {
   const CustomAddAndDecreaseItem({super.key});
 
   @override
+  State<CustomAddAndDecreaseItem> createState() =>
+      _CustomAddAndDecreaseItemState();
+}
+
+class _CustomAddAndDecreaseItemState extends State<CustomAddAndDecreaseItem> {
+  int text = 1;
+  @override
   Widget build(BuildContext context) {
     return Row(
-                children: [
-                IconButton(onPressed: (){}, icon: Icon(Icons.remove,size: 32.sp,color: kPrimaryColor,)),
-                  Container(
-                  width: 45.67.w,
-                    height: 45.67.w,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey.shade400),
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: Text('1',style: TextStyle(fontSize: 20.sp),),
-                  ),
-                  Icon(Icons.add,size: 32.sp,color: kPrimaryColor,),
-                ],
-              );
+      children: [
+        IconButton(
+            onPressed: () {
+              text >0 ? text-- : null;
+              setState(() {
+                
+              });
+            },
+            icon: Icon(
+              Icons.remove,
+              size: 32.sp,
+              color: kPrimaryColor,
+            )),
+        Container(
+          width: 45.67.w,
+          height: 45.67.w,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey.shade400),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Text(
+            '$text',
+            style: TextStyle(fontSize: 20.sp),
+          ),
+        ),
+        IconButton(onPressed: (){
+          text++;
+          setState(() {
+            
+          });
+        }, icon: Icon(Icons.add,size: 32.sp,color: kPrimaryColor,))
+      ],
+    );
   }
 }
