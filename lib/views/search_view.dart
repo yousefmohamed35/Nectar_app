11f99f2b6b879a314ab7ba6f2844e514a3ece0b5
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-05-07 23:16:33 +0300

    finish search view app bar

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nectarapp/widgets/custom_search_bar.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
@@ -6,8 +7,20 @@ class SearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    
-      
+     appBar: AppBar(
+      title: const CustomSearchBar(),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset('assets/images/pagefilter.png'),
+        )
+      ],
+     ),
+     body: GridView.count(crossAxisCount: 2,
+     children: [
+      C
+     ],
+     ),
     );
   }
 }
