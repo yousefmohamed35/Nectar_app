97a7b9f375766ffcb2e25a2e849fc391877120f2
yousefmohamed35 <yousefmo2021@gmail.com>
2024-06-08 18:39:15 +0300

    finish the favourite view

--- lib/views/favourite_view.dart
+++ lib/views/favourite_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nectarapp/widgets/custom_button.dart';
+import 'package:nectarapp/widgets/description_text.dart';
 import 'package:nectarapp/widgets/header_text.dart';
 
 class FavouriteView extends StatelessWidget {
@@ -20,15 +22,62 @@ class FavouriteView extends StatelessWidget {
           ),
         ),
       ),
-        body: ListView(
-          children: [
-            Row(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.w),
+        child: ListView(
+          children: List.generate(
+            8,
+            (index) => Column(
               children: [
-               Image.asset('assets/images/Sprite.png')
+                SizedBox(height: 30.h),
+                const CustomFavouritCard(),
+                SizedBox(height: 30.h),
+                const Divider(),
               ],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(16.0.sp),
+        child: const CustomButton(text: 'Add All To The Cart'),
+      ),
+    );
+  }
+}
+
+class CustomFavouritCard extends StatelessWidget {
+  const CustomFavouritCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Image.asset('assets/images/Sprite.png'),
+            SizedBox(width: 30.w),
+            const Column(
+              children: [
+                HeaderText(text: 'Sprite'),
+                DescriptionText(text: '325ml, Price')
+              ],
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            const HeaderText(text: '\$1.50'),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.arrow_forward_ios),
             )
           ],
         ),
+      ],
     );
   }
 }
