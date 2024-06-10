8e4a8410d8f253bcbe193ac59b86f966a042123c
yousefmohamed35 <yousefmo2021@gmail.com>
2024-06-10 22:32:53 +0300

    create accept order view

---
+++ lib/views/accept_order.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AcceptOrderView extends StatelessWidget {
+  const AcceptOrderView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
